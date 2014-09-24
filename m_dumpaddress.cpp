77b882b054659441d3634164d7cb0e15fe788ddc
john <john@john-VirtualBox.(none)>
2014-09-24 16:51:07 +0800

    read pwd dir (not ~/.bitcoin/peers.dat)

--- m_dumpaddress.cpp
+++ m_dumpaddress.cpp
@@ -1,6 +1,7 @@
 #include "net.h"
 #include "addrman.h"
 #include "netbase.h"
+#include <boost/filesystem.hpp>
 
 //change constructor
 class CAddrDB_m : public CAddrDB
@@ -10,9 +11,106 @@ private:
 public:
 	CAddrDB_m()
 	{
-		pathAddr = "./peers.dat";
+		pathAddr = "peers.dat";
 	}
+
+	//override to use pathAddr
+	bool Write(const CAddrMan& addr)
+	{
+    	// Generate random temporary filename
+    	unsigned short randv = 0;
+    	RAND_bytes((unsigned char *)&randv, sizeof(randv));
+    	std::string tmpfn = strprintf("peers.dat.%04x", randv);
+
+    	// serialize addresses, checksum data up to that point, then append csum
+    	CDataStream ssPeers(SER_DISK, CLIENT_VERSION);
+    	ssPeers << FLATDATA(Params().MessageStart());
+    	ssPeers << addr;
+    	uint256 hash = Hash(ssPeers.begin(), ssPeers.end());
+    	ssPeers << hash;
+
+    	// open temp output file, and associate with CAutoFile
+    	boost::filesystem::path pathTmp = GetDataDir() / tmpfn;
+    	FILE *file = fopen(pathTmp.string().c_str(), "wb");
+    	CAutoFile fileout = CAutoFile(file, SER_DISK, CLIENT_VERSION);
+    	if (!fileout)
+        	return error("%s : Failed to open file %s", __func__, pathTmp.string());
+
+    	// Write and commit header, data
+    	try {
+        	fileout << ssPeers;
+    	}
+    	catch (std::exception &e) {
+        	return error("%s : Serialize or I/O error - %s", __func__, e.what());
+    	}
+    	FileCommit(fileout);
+    	fileout.fclose();
+
+    	// replace existing peers.dat, if any, with new peers.dat.XXXX
+    	if (!RenameOver(pathTmp, pathAddr))
+        	return error("%s : Rename-into-place failed", __func__);
+
+    	return true;
+	}
+
+	
+	//override to use pathAddr
+	bool Read(CAddrMan& addr)
+	{
+    	// open input file, and associate with CAutoFile
+    	FILE *file = fopen(pathAddr.string().c_str(), "rb");
+    	CAutoFile filein = CAutoFile(file, SER_DISK, CLIENT_VERSION);
+    	if (!filein)
+        	return error("%s : Failed to open file %s", __func__, pathAddr.string());
+
+    	// use file size to size memory buffer
+    	int fileSize = boost::filesystem::file_size(pathAddr);
+    	int dataSize = fileSize - sizeof(uint256);
+    	// Don't try to resize to a negative number if file is small
+		if (dataSize < 0)
+      	  dataSize = 0;
+    	vector<unsigned char> vchData;
+    	vchData.resize(dataSize);
+    	uint256 hashIn;
+
+    	// read data and checksum from file
+    	try {
+        	filein.read((char *)&vchData[0], dataSize);
+        	filein >> hashIn;
+    	}
+    	catch (std::exception &e) {
+        	return error("%s : Deserialize or I/O error - %s", __func__, e.what());
+    	}
+    	filein.fclose();
+
+    	CDataStream ssPeers(vchData, SER_DISK, CLIENT_VERSION);
+
+    	// verify stored checksum matches input data
+    	uint256 hashTmp = Hash(ssPeers.begin(), ssPeers.end());
+    	if (hashIn != hashTmp)
+        	return error("%s : Checksum mismatch, data corrupted", __func__);
+
+    	unsigned char pchMsgTmp[4];
+    	try {
+        	// de-serialize file header (network specific magic number) and ..
+        	ssPeers >> FLATDATA(pchMsgTmp);
+
+        	// ... verify the network matches ours
+        	if (memcmp(pchMsgTmp, Params().MessageStart(), sizeof(pchMsgTmp)))
+            	return error("%s : Invalid network magic number", __func__);
+
+        	// de-serialize address data into one CAddrMan object
+        	ssPeers >> addr;
+    	}
+    	catch (std::exception &e) {
+        	return error("%s : Deserialize or I/O error - %s", __func__, e.what());
+    	}
+	
+    return true;
+	}
+	
 };
+
 
 int main()
 {
