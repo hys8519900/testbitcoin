f0568e87e70fbe5b94928632095b4aa0f823c595
john <john@john-VirtualBox.(none)>
2014-12-02 15:51:49 +0800

    add mineGenesis Block

--- block_change_mineGenesis.cpp
+++ block_change_mineGenesis.cpp
@@ -1,6 +1,7 @@
 #include "core.h"
 #include "util.h"
 #include <stdio.h>
+#include <iostream>
 
 using namespace std;
 
@@ -20,7 +21,7 @@ int main()
 	txNew.vin.resize(1);
 	txNew.vout.resize(1);
 	txNew.vin[0].scriptSig = CScript() << 486604799 << CBigNum(4) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
-	txNew.vout[0].nValue = 50 * COIN;
+	txNew.vout[0].nValue = 50 * 10000;
 	txNew.vout[0].scriptPubKey = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
 
 	CBlock genesis;
@@ -29,8 +30,15 @@ int main()
 	genesis.hashMerkleRoot = genesis.BuildMerkleTree();
 	genesis.nVersion = 1;
 	genesis.nTime = 1231006505;
-	genesis.nBits = 0x1d00ffff;
-	genesis.nNonce = 2083236893;
+	genesis.nBits = 0x200fffff;
+	genesis.nNonce = 1;
 
+	CBigNum bnTarget;
+	bnTarget.SetCompact(genesis.nBits);
+	while(genesis.GetHash() > bnTarget.getuint256())
+	{
+		genesis.nNonce += 1;	
+	}
+	cout << "nNonce: " << genesis.nNonce << endl;
 	printf("genesis hash: %s\n",genesis.GetHash().ToString().c_str());
 }
